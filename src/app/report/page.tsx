18e5ea735c4002b0e3cdf31ad5e157c05e14d61c
ivandrian11 <ivandriancode@gmail.com>
2025-06-20 23:22:46 +0800

    feat: implement report page layout with settings modal

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,3 +1,30 @@
+'use client'
+
+import SettingsModal from '@/components/modal/settings-modal'
+import { Button } from '@/components/ui/button'
+import { Settings } from 'lucide-react'
+import { useState } from 'react'
+
 export default function Report() {
-  return <></>
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false)
+
+  return (
+    <>
+      <div className='sticky top-0 bg-background z-10 px-4 py-4 flex justify-between items-center'>
+        <h1 className='text-xl font-bold'>Report</h1>
+        <Button
+          variant='ghost'
+          size='icon'
+          onClick={() => setIsSettingsOpen(true)}
+        >
+          <Settings className='!h-5 !w-5' />
+        </Button>
+      </div>
+
+      <SettingsModal
+        isOpen={isSettingsOpen}
+        onClose={() => setIsSettingsOpen(false)}
+      />
+    </>
+  )
 }
