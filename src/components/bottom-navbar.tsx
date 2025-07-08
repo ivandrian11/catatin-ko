49ad03074f4cca97e1b75e7ca6bdf284ae02287f
ivandrian11 <ivandriancode@gmail.com>
2025-07-08 23:04:23 +0800

    feat: enhance styling for dark mode in bottom navbar, accordion, and switch components

--- src/components/bottom-navbar.tsx
+++ src/components/bottom-navbar.tsx
@@ -20,9 +20,9 @@ export const BottomNavbar = () => {
       <div className='fixed bottom-0 left-0 right-0 h-16 bg-background border-t flex items-center justify-around z-20'>
         <Button
           variant='ghost'
-          className={`nav-tab flex-1 h-full hover:text-yellow-700 ${
+          className={`nav-tab flex-1 h-full hover:text-yellow-700 dark:hover:text-yellow-500 ${
             pathname === '/'
-              ? 'border-t-2 border-yellow-600 text-yellow-700'
+              ? 'border-t-2 border-yellow-600 text-yellow-700 dark:text-yellow-500'
               : ''
           }`}
           onClick={() => router.push('/')}
@@ -32,7 +32,14 @@ export const BottomNavbar = () => {
 
         <Button
           variant='ghost'
-          className='border-2 border-white w-14 h-14 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full bg-yellow-700 hover:bg-yellow-800 hover:text-white text-white flex items-center justify-center shadow-lg'
+          className='
+            w-14 h-14 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2
+            rounded-full shadow-lg flex items-center justify-center
+            border-2 border-gray-200 dark:border-gray-700
+            bg-yellow-600 dark:bg-yellow-500
+            text-white
+            hover:bg-yellow-700 dark:hover:bg-yellow-600
+          '
           onClick={handleAddTransaction}
         >
           <Plus className='!w-6 !h-6' />
@@ -40,9 +47,9 @@ export const BottomNavbar = () => {
 
         <Button
           variant='ghost'
-          className={`nav-tab flex-1 h-full hover:text-yellow-700 ${
+          className={`nav-tab flex-1 h-full hover:text-yellow-700 dark:hover:text-yellow-500 ${
             pathname === '/report'
-              ? 'border-t-2 border-yellow-600 text-yellow-700'
+              ? 'border-t-2 border-yellow-600 text-yellow-700 dark:text-yellow-500'
               : ''
           }`}
           onClick={() => router.push('/report')}
